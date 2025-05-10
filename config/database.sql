8a0c2d5a8a5684cc1fd785cf13046122a7c565f7
Moises Theo Atienza <theomacatienza@gmail.com>
2025-05-11 02:54:04 +0800

    Update database.sql

--- config/database.sql
+++ config/database.sql
@@ -8,6 +8,7 @@ CREATE TABLE users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    profile_picture VARCHAR(255) DEFAULT 'assets/images/profile.avif',
     is_admin BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
