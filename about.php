535fbdd54b4b90d8df19ed548c66e20ea523007d
Moises Theo Atienza <theomacatienza@gmail.com>
2025-05-13 13:25:13 +0800

    Update about.php

--- about.php
+++ about.php
@@ -9,6 +9,7 @@ require_once 'config/database.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - CineVerse</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/svg+xml" href="assets/images/logo-cineverse.svg">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
