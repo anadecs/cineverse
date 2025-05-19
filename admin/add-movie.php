8973f2965dfdf24df798b89c8a1e6a2d10731c86
Moises Theo Atienza <theomacatienza@gmail.com>
2025-05-19 18:13:13 +0800

    Update add-movie.php

--- admin/add-movie.php
+++ admin/add-movie.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once '../config/database.php';
-require_once '../config/populate.php';
+require_once '../config/tmdb.php';
 
 // Check if user is admin
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['is_admin']) || !$_SESSION['is_admin']) {
